c9356acb40a3c15002041955c46c90b2ea967e8e
asasia1935 <sungmin1935@naver.com>
2023-04-05 05:00:41 +0900

    Ranking Component + Applying Component

--- src/components/SectionTop.js
+++ src/components/SectionTop.js
@@ -5,10 +5,11 @@ import week2 from "../assets/week2.png";
 import week3 from "../assets/week3.png";
 import week4 from "../assets/week4.png";
 import week5 from "../assets/week5.png";
+import Ranking from "./Ranking";
 
 const Wrapper = styled.div`
     width: 90%;
-    height: 300px;
+    height: 400px;
     background-color: #F7F7F7;
     margin-left: auto;
     margin-right: auto;
@@ -49,7 +50,7 @@ const SectionTop = () =>{
         <Wrapper>
             <Title>이 주의 영양제 Pick!</Title>
             <Container>
-                <WeekList>
+                {/* <WeekList>
                     <Pill src={week1}/>
                     <Text>멀티비타민 오쏘뮬 이뮨</Text>
                 </WeekList>
@@ -69,7 +70,13 @@ const SectionTop = () =>{
                     <Pill src={week5}/>
                     <Text>종근당 칼슘 앤 마그네슘</Text>
                 </WeekList>
-                </Container>
+                */}
+                <Ranking toLink="/condition" Ranking={1} imgSrc={week1} itemName="멀티비타민 오쏘뮬 이뮨" />
+                <Ranking toLink="/condition" Ranking={2} imgSrc={week2} itemName="락토핏 골드" />
+                <Ranking toLink="/condition" Ranking={3} imgSrc={week3} itemName="뉴트리코어 밀크씨슬" />
+                <Ranking toLink="/condition" Ranking={4} imgSrc={week4} itemName="루테인 지아잔틴" />
+                <Ranking toLink="/condition" Ranking={5} imgSrc={week5} itemName="종근당 칼슘 앤 마그네슘" />
+            </Container>
         </Wrapper>
     );
 }

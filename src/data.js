7c62845c9f0f469fedc6c59847ab070a8d43a8d5
simple <shwncho4@gmail.com>
2023-04-09 15:03:59 +0900

    refactor: data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,12 @@
-const Datas = [
+export default [
     {
         id : 0,
         title : "고려은단 멀티비타민 올인원",
         content : "비타민 13종과 미네랄 10종으로 건강 기능에 도움을 줄 수 있습니다.",
         usage : "1일 1회, 1회 1정 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : "https://brand.naver.com/koreaeundanhc/products/5288465421NaPm=ct%3Dlg0n7ieg%7Cci%3D90718562ae2da8cfa8ac563ca34a2d971f0aa63e%7Ctr%3Dslcf%7Csn%3D1444796%7Chk%3Dde2df7fb9abcc89167674278c963591631de3fd1"
     },
 
     {
@@ -14,7 +15,8 @@ const Datas = [
         content : "비타민&미네랄 12종 함유와 부원료 45가지로 건강 기능에 도움을 줄 수 있습니다.",
         usage : "1일 1회, 1회 6정 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : "https://brand.naver.com/nutricore/products/384509110?NaPm=ct%3Dlg0na2zs%7Cci%3Dd49b6b7e2ebc328f1011c3ce67f12e61123dbe73%7Ctr%3Dslcf%7Csn%3D358231%7Chk%3D155f6b517cf367b548df2885c95a9db6d9666d5e"
     },
 
     {
@@ -23,7 +25,8 @@ const Datas = [
         content : "21가지 비타민과 미네랄과 54가지 보조성분으로 건강 기능에 도움을 줄 수 있습니다.",
         usage : "1일 1회, 1회 1정 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -32,7 +35,8 @@ const Datas = [
         content : "한국 성인 여성에게 필요한 영양을 고려한 22가지의 비타민과 미네랄로 건강 기능에 도움을 줄 수 있습니다.",
         usage : "1일 1회, 1회 1정 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -41,7 +45,8 @@ const Datas = [
         content : "독일 의사가 추천하는 명품 비타민으로 건강 기능에 도움을 줄 수 있습니다.",
         usage : "1일 1회, 1회 1포(리퀴드 1병, 2타블렛) 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -50,7 +55,8 @@ const Datas = [
         content : "살아있는 유산균을 이용해 장건강에 도움이 될 수 있습니다.",
         usage : "1일 1회, 1회 1포 복용",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -59,7 +65,8 @@ const Datas = [
         content : "",
         usage : "",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -68,7 +75,8 @@ const Datas = [
         content : "",
         usage : "",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -77,7 +85,8 @@ const Datas = [
         content : "",
         usage : "",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 
     {
@@ -86,8 +95,7 @@ const Datas = [
         content : "",
         usage : "",
         product : "제품 어쩌구",
-        ingredient : "성분 어쩌구"
+        ingredient : "성분 어쩌구",
+        buyurl : ""
     },
 ]
-
-export default Datas;
